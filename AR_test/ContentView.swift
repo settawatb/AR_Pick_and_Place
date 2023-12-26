755f8f94d971b9d51c8469c9159b280e6a602989
settawatb <settawat.bpalm@gmail.com>
2023-12-26 22:49:46 +0700

    Improve AR Object Placement

--- AR_test/ContentView.swift
+++ AR_test/ContentView.swift
@@ -6,58 +6,70 @@
 //
 
 import SwiftUI
+import ARKit
 import RealityKit
+import FocusEntity
+
 
 struct ContentView : View {
     @State private var isPlacementEnabled = false
-    @State private var selectedModel: String?
-    @State private var modelConfirmedForPlacement: String?
+    @State private var selectedModel: Model?
+    @State private var modelConfirmedForPlacement: Model?
     
-    private var models: [String] = {
+    private var models: [Model] = {
             let filemanager = FileManager.default
             guard let path = Bundle.main.resourcePath, let files = try? filemanager.contentsOfDirectory(atPath:path) else {
                 return []
             }
-            var availableModels: [String] = []
+            var availableModels: [Model] = []
             for filename in files where filename.hasSuffix("usdz") {
                 let modelName = filename.replacingOccurrences(of:".usdz", with: "")
-                availableModels.append(modelName)
+                let model = Model(modelName: modelName)
+                
+                availableModels.append(model)
             }
             return availableModels
         }()
     
     
     var body: some View {
-        ZStack(alignment: .bottom) {
-            
-            ARViewContainer(modelConfirmedForPlacement: self.$modelConfirmedForPlacement)
-            
-            if self.isPlacementEnabled {
-                PlacementButtonsView(isPlacementEnabled: self.$isPlacementEnabled, selectedModel: self.$selectedModel, modelConfirmedForPlacement: self.$modelConfirmedForPlacement)
-                
-            } else {
-                ModelPickerView(isPlacementEnabled: self.$isPlacementEnabled, selectedModel: self.$selectedModel, models: self.models)
+            ZStack(alignment: .bottom) {
+                ARViewContainer(modelConfirmedForPlacement: self.$modelConfirmedForPlacement)
+
+                if self.isPlacementEnabled {
+                    PlacementButtonsView(isPlacementEnabled: self.$isPlacementEnabled, selectedModel: self.$selectedModel, modelConfirmedForPlacement: self.$modelConfirmedForPlacement)
+                } else {
+                    ModelPickerView(isPlacementEnabled: self.$isPlacementEnabled, selectedModel: self.$selectedModel, models: self.models)
+                }
             }
-            
         }
-    }
 }
 
 struct ARViewContainer: UIViewRepresentable {
     
-    @Binding var modelConfirmedForPlacement: String?
+    @Binding var modelConfirmedForPlacement: Model?
     
     func makeUIView(context: Context) -> ARView {
         
-        let arView = ARView(frame: .zero)
+//        let arView = ARView(frame: .zero)
+        let arView = CustomARView(frame: .zero)
 
         return arView
         
     }
     
     func updateUIView(_ uiView: ARView, context: Context) {
-        if let modelName = self.modelConfirmedForPlacement {
-            print("DEBUG: adding model to scene - \(modelName)")
+        if let model = self.modelConfirmedForPlacement {
+            
+            if let modelEntity = model.modelEntity {
+                print("DEBUG: adding model to scene - \(model.modelName)")
+                
+                let anchorEntity = AnchorEntity(plane: .any)
+                anchorEntity.addChild(modelEntity)
+                uiView.scene.addAnchor(anchorEntity)
+            } else {
+                print("DEBUG: Unable to load modelEntity for \(model.modelName)")
+            }
             
             DispatchQueue.main.async {
                 self.modelConfirmedForPlacement = nil
@@ -67,12 +79,62 @@ struct ARViewContainer: UIViewRepresentable {
     
 }
 
+
+class CustomARView: ARView {
+    
+    var focusSquare: FocusEntity!
+    
+    required init(frame frameRect: CGRect) {
+            super.init(frame: frameRect)
+            setupFocusSquare()
+            self.setupARView()
+        }
+
+        required init(coder decoder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+    
+    func setupFocusSquare() {
+        focusSquare = FocusEntity(on: self, focus: .classic)
+        focusSquare.delegate = self
+        focusSquare.setAutoUpdate(to: true)
+    }
+    
+    func setupARView() {
+        let config = ARWorldTrackingConfiguration()
+        config.planeDetection = [.horizontal, .vertical]
+        config.environmentTexturing = .automatic
+        
+        if ARWorldTrackingConfiguration.supportsSceneReconstruction(.mesh) {
+            config.sceneReconstruction = .mesh
+        }
+        
+        self.session.run(config)
+    }
+}
+
+extension CustomARView: FocusEntityDelegate {
+    func focusEntity(_ entity: FocusEntity, trackingUpdated newState: FocusEntity.State, oldState: FocusEntity.State?) {
+        print("Tracking state updated")
+    }
+    
+    func toInitializingState() {
+        print("Initializing state")
+    }
+}
+
+
+
+
+
+
+
 struct ModelPickerView: View {
     @Binding var isPlacementEnabled: Bool
-    @Binding var selectedModel: String?
+    @Binding var selectedModel: Model?
     
     
-    var models: [String]
+    var models: [Model]
     
     var body: some View {
         
@@ -80,12 +142,12 @@ struct ModelPickerView: View {
             HStack(spacing: 30) {
                 ForEach(self.models.indices, id: \.self) { index in
                     Button(action: {
-                        print("DEBUG: selected model with name: \(self.models[index])")
+                        print("DEBUG: selected model with name: \(self.models[index].modelName)")
                         
                         self.selectedModel = self.models[index]
                         self.isPlacementEnabled = true
                     }) {
-                        Image(uiImage: UIImage(named: self.models[index])!)
+                        Image(uiImage: self.models[index].image)
                             .resizable()
                             .frame(height: 80)
                             .aspectRatio(1/1, contentMode: .fit)
@@ -104,8 +166,8 @@ struct ModelPickerView: View {
 
 struct PlacementButtonsView: View {
     @Binding var isPlacementEnabled: Bool
-    @Binding var selectedModel: String?
-    @Binding var modelConfirmedForPlacement: String?
+    @Binding var selectedModel: Model?
+    @Binding var modelConfirmedForPlacement: Model?
     
     var body: some View {
         HStack {
@@ -150,6 +212,10 @@ struct PlacementButtonsView: View {
 
 
 
-#Preview {
-    ContentView()
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
+#endif
--- AR_test/Model_products.swift
+++ AR_test/Model_products.swift
@@ -5,4 +5,31 @@
 //  Created by Kisses MJ on 26/12/2566 BE.
 //
 
-import Foundation
+import UIKit
+import RealityKit
+import Combine
+
+class Model {
+    var modelName: String
+    var image: UIImage
+    var modelEntity: ModelEntity?
+    
+    private var cancellable: AnyCancellable? = nil
+    
+    init(modelName: String) {
+        self.modelName = modelName
+        self.image = UIImage(named: modelName)!
+        
+        let filename = modelName + ".usdz"
+        self.cancellable = ModelEntity.loadModelAsync(named: filename)
+            .sink(receiveCompletion: {loadCompletion in
+                // Handle error
+                print("DEBUG: Unable to load modelEntity for modelName: \(self.modelName)")
+            }, receiveValue: { modelEntity in
+                // Get modelEntity
+                self.modelEntity = modelEntity
+                print("DEBUG: Successfully loaded modelEntity for modelName: \(self.modelName)")
+            })
+        
+    }
+}
